31b6c9267d0b62a327bd2a5c30800e5995c96d06
Emil155rf <levissctt@yandex.ru>
2024-10-09 22:28:47 +0300

    upd

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
 const popup = document.getElementById('popup')
 const closeBtn = document.getElementById('closeBtn')
 const login = document.getElementById('login')
-const close_btn = document.getElementById('close_Btn')
-const open = document.getElementById('header__login')
-const overlay = document.getElementById('overlay')
 
 const mapOpen = document.querySelector('.btn__1')
 const map = document.querySelector('.card-map')
@@ -19,6 +16,41 @@ const closeFilter = document.querySelector('.filter-close_Btn')
 
 const credit = document.querySelector('.cred')
 const scrollTo = document.querySelector('.forbes__h1')
+
+
+
+let slider = document.getElementById("myRange")
+let output = document.getElementById("value")
+output.innerHTML = slider.value
+
+
+let slider_2 = document.getElementById("myRange-2")
+let output_2 = document.getElementById("value-2")
+output_2.innerHTML = slider_2.value
+
+let slider_3 = document.getElementById("myRange-3")
+let output_3 = document.getElementById("value-3")
+output_3.innerHTML = slider_3.value
+
+let slider_4 = document.getElementById("myRange-4")
+let output_4 = document.getElementById("value-4")
+output_4.innerHTML = slider_4.value
+
+let slider_5 = document.getElementById("myRange-5")
+let output_5 = document.getElementById("value-5")
+output_5.innerHTML = slider_5.value
+
+let slider_6 = document.getElementById("myRange-6")
+let output_6 = document.getElementById("value-6")
+output_6.innerHTML = slider_6.value
+
+
+
+let slider_7 = document.getElementById("myRange-7")
+let output_7 = document.getElementById("value-7")
+output_7.innerHTML = slider_7.value
+
+
 
 
 
@@ -70,101 +102,31 @@ window.onload = function () {
 
 
 
-function showLogin() {
-    open.addEventListener('click', () => {
-        login.style.display = 'block'
-        close_btn.style.display = 'block'
-        overlay.style.display = 'block'
-
-    })
-
-
-    close_btn.addEventListener('click', () => {
-        login.style.display = 'none'
-        close_btn.style.display = 'none'
-        overlay.style.display = 'none'
-
-    })
-
-}
-
-showLogin()
 
 
 
 
 
-
-
-
-
-
-let slider = document.getElementById("myRange")
-let output = document.getElementById("value")
-output.innerHTML = slider.value
-
-slider.oninput = function () {
-    output.innerHTML = this.value
-}
-
-
-
-
-let slider_2 = document.getElementById("myRange-2")
-let output_2 = document.getElementById("value-2")
-output_2.innerHTML = slider_2.value
-
-slider_2.oninput = function () {
-    output_2.innerHTML = this.value
-}
-
-let slider_3 = document.getElementById("myRange-3")
-let output_3 = document.getElementById("value-3")
-output_3.innerHTML = slider_3.value
-
-slider_3.oninput = function () {
-    output_3.innerHTML = this.value
-}
-
-
-
-let slider_4 = document.getElementById("myRange-4")
-let output_4 = document.getElementById("value-4")
-output_4.innerHTML = slider_4.value
-
-slider_4.oninput = function () {
-    output_4.innerHTML = this.value
-}
-
-let slider_5 = document.getElementById("myRange-5")
-let output_5 = document.getElementById("value-5")
-output_5.innerHTML = slider_5.value
-
-slider_5.oninput = function () {
-    output_5.innerHTML = this.value
+function sld(val1, val2) {
+    val1.oninput = function() {
+        val2.innerHTML = this.value
+    }
 }
 
 
 
 
 
-let slider_6 = document.getElementById("myRange-6")
-let output_6 = document.getElementById("value-6")
-output_6.innerHTML = slider_6.value
+sld(slider, output)
+sld(slider_2, output_2)
+sld(slider_3, output_3)
+sld(slider_4, output_4)
+sld(slider_5, output_5)
+sld(slider_6, output_6)
+sld(slider_7, output_7)
 
-slider_6.oninput = function () {
-    output_6.innerHTML = this.value
-}
 
 
-
-let slider_7 = document.getElementById("myRange-7")
-let output_7 = document.getElementById("value-7")
-output_7.innerHTML = slider_7.value
-
-slider_7.oninput = function () {
-    output_7.innerHTML = this.value
-}
 
 
 
